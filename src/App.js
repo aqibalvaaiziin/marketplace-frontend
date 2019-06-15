7771ce1ff45eaa5c139502581572d35f578f5908
diosyahrizal <diosyahrizal@gmail.com>
2019-06-15 16:11:48 +0700

    create page detail produk

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Page1 from './pages/Pages1/Page1'
-import Page2 from './pages/Pages2/Page2'
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Page1 from "./pages/Pages1/Page1";
+import Page2 from "./pages/Pages2/Page2";
+import DetailProduct from "./pages/DetailProduct/DetailProduct";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Home,
-    name: 'home',
-    label: 'Home'
+    name: "home",
+    label: "Home"
   },
   {
-    path: '/page1',
+    path: "/page1",
     component: Page1,
-    name: 'page1',
-    label: 'Page 1'
+    name: "page1",
+    label: "Page 1"
   },
   {
-    path: '/page2',
+    path: "/page2",
     component: Page2,
-    name: 'page2',
-    label: 'Page 2'
+    name: "page2",
+    label: "Page 2"
+  },
+  {
+    path: "/detail-produk",
+    component: DetailProduct,
+    name: "detailProduk",
+    label: "Detail Produk"
   }
-]
+];
 
 export default class App extends Component {
-  state = {}
+  state = {};
 
   changeActiveRoute(name) {
-    this.setState({ activeItem: name })
+    this.setState({
+      activeItem: name
+    });
   }
 
   isActive(route) {
     return (
       this.state.activeItem === route.name ||
       window.location.pathname === route.path
-    )
+    );
   }
 
   render() {
@@ -45,7 +54,7 @@ export default class App extends Component {
       <div>
         <Router>
           <Menu>
-            {routes.map(route => (
+            {routes.map((route, index) => (
               <Link to={route.path}>
                 <Menu.Item
                   name={route.name}
@@ -57,12 +66,11 @@ export default class App extends Component {
               </Link>
             ))}
           </Menu>
-
           {routes.map(route => (
             <Route path={route.path} exact component={route.component} />
           ))}
         </Router>
       </div>
-    )
+    );
   }
 }
--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Card, Icon, Image, Button } from 'semantic-ui-react';
-
+import { Link } from 'react-router-dom'
 class CardProduct extends Component {
     constructor(props) {
         super(props);
@@ -13,9 +13,12 @@ class CardProduct extends Component {
                 <Card>
                     <Image src="https://placeimg.com/250/200/tech" />
                     <Card.Content>
-                        <Card.Header>{this.props.name}</Card.Header>
+                        <Link to={"/detailproduct/"}>
+                            <Card.Header>{this.props.name}</Card.Header>
+                        </Link>
                         <Card.Description>Rp.{this.props.price}</Card.Description>
                     </Card.Content>
+
                     <Card.Content>
                         <Button primary fluid><Icon name="shopping cart" />Add to Chart</Button>
                     </Card.Content>
