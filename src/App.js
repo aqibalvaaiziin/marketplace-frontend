b9bb792db5805ddc5c361d32897f0cbb3a5ecc49
mnindrazaka <mnindrazaka@gmail.com>
2019-06-16 07:43:34 +0700

    link beranda to detail produk

--- src/App.js
+++ src/App.js
@@ -1,96 +1,90 @@
-import React, { Component } from "react";
-import { Menu,Icon } from "semantic-ui-react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Page1 from "./pages/Pages1/Page1";
-import Page2 from "./pages/Pages2/Page2";
-import DetailProduct from "./pages/DetailProduct/DetailProduct";
+import React, { Component } from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Beranda from './pages/Beranda'
+import DetailProduk from './pages/DetailProduk'
 import Keranjang from './pages/Keranjang'
 
 const routes = [
   {
-    path: "/",
-    component: Home,
-    name: "home",
-    label: "Home"
-  },
-  {
-    path: "/page1",
-    component: Page1,
-    name: "page1",
-    label: "Page 1"
-  },
-  {
-    path: "/page2",
-    component: Page2,
-    name: 'page2',
-    label: 'Page 2'
+    path: '/',
+    component: Beranda,
+    name: 'beranda',
+    label: 'Beranda'
   },
   {
     path: '/keranjang',
     component: Keranjang,
     name: 'keranjang',
-    label: 'Keranjang'
+    label: 'Keranjang',
+    hide: true
   },
   {
-    path: "/detail-produk",
-    component: DetailProduct,
-    name: "detailProduk",
-    label: "Detail Produk"
-  },
-];
+    path: '/detail-produk',
+    component: DetailProduk,
+    name: 'detailProduk',
+    label: 'Detail Produk',
+    hide: true
+  }
+]
 
 export default class App extends Component {
-  state = {};
+  state = {}
 
   changeActiveRoute(name) {
     this.setState({
       activeItem: name
-    });
+    })
   }
 
   isActive(route) {
     return (
       this.state.activeItem === route.name ||
       window.location.pathname === route.path
-    );
+    )
   }
 
   render() {
     return (
       <div>
         <Router>
-          <Menu>
-            {routes.map((route, index) => (
-              <Link to={route.path}>
-                <Menu.Item
-                  name={route.name}
-                  active={this.isActive(route)}
-                  onClick={(e, { name }) => this.changeActiveRoute(name)}
-                >
-                  {route.label}
+          <Menu fixed="top">
+            {routes.map(
+              route =>
+                !route.hide && (
+                  <Link to={route.path}>
+                    <Menu.Item
+                      name={route.name}
+                      active={this.isActive(route)}
+                      onClick={(e, { name }) => this.changeActiveRoute(name)}
+                    >
+                      {route.label}
+                    </Menu.Item>
+                  </Link>
+                )
+            )}
+            <Menu.Menu position="right">
+              <Link to="/keranjang">
+                <Menu.Item>
+                  <Icon name="cart" />
                 </Menu.Item>
               </Link>
-            ))}
-            {
-              routes.hide ?
-              false
-              : true
-            }
-          <Menu.Menu position='right'>
-          <Link to ="/keranjang">
-            <Menu.Item>
-              <Icon name="cart" />
-            </Menu.Item>
-          </Link>
-        </Menu.Menu>
-            
+            </Menu.Menu>
           </Menu>
-          {routes.map(route => (
-            <Route path={route.path} exact component={route.component} />
-          ))}
+
+          <div style={styles.pageContainer}>
+            {routes.map(route => (
+              <Route path={route.path} exact component={route.component} />
+            ))}
+          </div>
         </Router>
       </div>
-    );
+    )
+  }
+}
+
+const styles = {
+  pageContainer: {
+    marginTop: 50
   }
 }
---
+++ src/pages/Beranda/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import ProductCard from './ProductCard'
+import axios from 'axios'
+
+export default class Home extends Component {
+  state = {
+    produk: []
+  }
+
+  componentDidMount() {
+    axios.get('https://marketplace-express.herokuapp.com/produk').then(res => {
+      this.setState({
+        produk: res.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Grid columns={5}>
+          <Grid.Row>
+            {this.state.produk.map(produk => {
+              return (
+                <Grid.Column style={styles.cardRow}>
+                  <Link to={{ pathname: '/detail-produk', state: produk }}>
+                    <ProductCard
+                      key={produk.id_produk}
+                      name={produk.nama}
+                      price={produk.harga}
+                    />
+                  </Link>
+                </Grid.Column>
+              )
+            })}
+          </Grid.Row>
+        </Grid>
+      </Container>
+    )
+  }
+}
+
+const styles = {
+  cardRow: {
+    marginTop: 15
+  }
+}
