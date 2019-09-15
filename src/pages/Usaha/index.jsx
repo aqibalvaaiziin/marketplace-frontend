6f13b76011c374649cbaa68eabb0a70c13b7cb21
Daffa Akbar <daffaputradamar@gmail.com>
2019-09-15 16:22:44 +0700

    fix produk and usaha integration

--- src/pages/Usaha/index.jsx
+++ src/pages/Usaha/index.jsx
@@ -1,64 +1,85 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import ProductCard from '../Beranda/ProductCard'
 import { Container, Image, Grid, Header, Segment, Divider } from 'semantic-ui-react'
+import { UserContext } from '../../App'
 import axios from 'axios'
 
 
-function Usaha() {
-    const [kumpulanProduk, setKumpulanProduk] = useState([])
+function Usaha(props) {
+    const context = useContext(UserContext)
+
+    const [usaha, setUsaha] = useState()
 
     useEffect(() => {
-        axios
-        .get('http://localhost:8000/produk')
-        .then(res => setKumpulanProduk(res.data))
+        console.log(props.location)
+        if (!props.location.state) {
+            const idUsaha = context.getPengguna().usaha.id_usaha
+            axios
+            .get(`http://localhost:8000/usaha/${idUsaha}`)
+            .then(res => {
+                setUsaha(res.data)
+            })
+        } else {
+            axios
+            .get(`http://localhost:8000/usaha/${props.location.state}`)
+            .then(res => setUsaha(res.data))
+        }
     }, [])
 
     return (
         <Container style={styles.marginCard}>
             <Segment>
-                <Grid divided columns={2} >
-                    <Grid.Column width="2">
-                        <Image
-                            src="https://placeimg.com/120/120/any" 
-                            fluid
-                        />
-                    </Grid.Column>
-                    <Grid.Column width="14">
-                        <Grid celled="internally" columns={2}>
-                            <Grid.Column>
-                                <Header size="medium">DaffaAkbar</Header>
-                                <Header sub style={styles.noMargin}>Ini Slogan Saya masih placeholder yang penting maju jangan mundur ya</Header>
+                {
+                    usaha && (
+                        <Grid divided columns={2} >
+                            <Grid.Column width="2">
+                                <Image
+                                    src="https://placeimg.com/120/120/any" 
+                                    fluid
+                                />
                             </Grid.Column>
-                            <Grid.Column>
-                                <Header
-                                    size="small"
-                                    icon="map marker alternate"
-                                    content="Alamat"
-                                    subheader="Bla Bla Bla"
-                                />
-                                <Header
-                                    size="small"
-                                    icon="phone"
-                                    content="Nomor Telepon"
-                                    subheader="086765432123"
-                                />
+                            <Grid.Column width="14">
+                                <Grid celled="internally" columns={2}>
+                                    <Grid.Column>
+                                        <Header size="medium">{usaha.nama}</Header>
+                                        <Header sub style={styles.noMargin}>Ini Slogan Saya masih placeholder yang penting maju jangan mundur ya</Header>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Header
+                                            size="small"
+                                            icon="map marker alternate"
+                                            content="Alamat"
+                                            subheader="Bla Bla Bla"
+                                        />
+                                        <Header
+                                            size="small"
+                                            icon="phone"
+                                            content="Nomor Telepon"
+                                            subheader={usaha.no_telp}
+                                        />
+                                    </Grid.Column>
+                                </Grid>
                             </Grid.Column>
                         </Grid>
-                    </Grid.Column>
-                </Grid>
+                    )
+                }
             </Segment>
             <Divider horizontal style={styles.marginDivider}>Produk Usaha</Divider>
             <Grid columns={5}>
-                <Grid.Row>
-                {kumpulanProduk.map(produk => (
-                    <Grid.Column style={styles.cardRow} key={produk.id_produk}>
-                    <Link to={{ pathname: '/detail-produk', state: produk }}>
-                        <ProductCard name={produk.nama} price={produk.harga} />
-                    </Link>
-                    </Grid.Column>
-                ))}
-                </Grid.Row>
+                {
+                    usaha && (
+                        <Grid.Row>
+                            {usaha.produks.map(produk => (
+                                <Grid.Column style={styles.cardRow} key={produk.id_produk}>
+                                <Link to={{ pathname: '/detail-produk', state: produk }}>
+                                    <ProductCard name={produk.nama} price={produk.harga} />
+                                </Link>
+                                </Grid.Column>
+                            ))}
+                        </Grid.Row>
+                    )
+                }
             </Grid>
         </Container>
     )
