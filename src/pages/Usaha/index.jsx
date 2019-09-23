220d7259e553e5412e758dc6ce872d585851f20f
Daffa Akbar <daffaputradamar@gmail.com>
2019-09-23 09:45:50 +0700

    hide button tambah produk

--- src/pages/Usaha/index.jsx
+++ src/pages/Usaha/index.jsx
@@ -10,9 +10,10 @@ function Usaha(props) {
     const context = useContext(UserContext)
     let idUsaha
     const [usaha, setUsaha] = useState()
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        console.log(props.location)
+        setLoading(false)
         if (!props.location.state) {
             if (context.getPengguna().usaha) {
                 idUsaha = context.getPengguna().usaha.id_usaha
@@ -20,6 +21,7 @@ function Usaha(props) {
                 .get(`http://localhost:8000/usaha/${idUsaha}`)
                 .then(res => {
                     setUsaha(res.data)
+                    setLoading(true)
                 })
             } else {
                 props.history.push('/')
@@ -27,15 +29,36 @@ function Usaha(props) {
         } else {
             axios
             .get(`http://localhost:8000/usaha/${props.location.state.id_usaha}`)
-            .then(res => setUsaha(res.data))
+            .then(res => {
+                setUsaha(res.data)
+                setLoading(true)
+            })
         }
     }, [])
+
+    function isLoggedIn() {
+        return context.isLoggedIn()
+      }
+    
+      function doesHaveUsaha() {
+        return isLoggedIn() && context.getPengguna().usaha
+      }
+    
+      function doesHaveSameUsahaId() {
+        if (doesHaveUsaha() && loading) {
+          const halamanUsaha = usaha.id_usaha
+          const userUsaha = context.getPengguna().usaha.id_usaha
+          return doesHaveUsaha() && halamanUsaha == userUsaha
+        } else {
+          return false
+        }
+      }
 
     return (
         <Container style={styles.marginCard}>
             <Segment>
                 {
-                    usaha && (
+                    usaha && loading && (
                         <Grid divided columns={2} >
                             <Grid.Column width="2">
                                 <Image
@@ -70,7 +93,13 @@ function Usaha(props) {
                 }
             </Segment>
             <Divider horizontal >Produk Usaha</Divider>
-            <Link to={{ pathname: '/tambahproduk'}}><Button color="green" icon style={styles.marginDivider}><Icon name="plus"></Icon> Tambah Produk</Button></Link>
+            {
+                doesHaveSameUsahaId() && (
+                    <Link to={{ pathname: '/tambahproduk'}}>
+                        <Button color="green" icon style={styles.marginDivider}><Icon name="plus"></Icon> Tambah Produk</Button>
+                    </Link>
+                )
+            }
             <Grid columns={5}>
                 {
                     usaha && (
