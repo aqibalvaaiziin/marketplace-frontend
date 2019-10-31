8ce13ff553c00e0693cb0f8ff1424f1483ed4c0a
Daffa Akbar <daffaputradamar@gmail.com>
2019-10-31 07:59:24 +0700

    menampilkan semua transaksi di pesanan saya

--- src/pages/Profil/AkunSaya.jsx
+++ src/pages/Profil/AkunSaya.jsx
@@ -1,57 +1,65 @@
-import React from 'react';
-import { Container, Segment, Header, Grid, Icon, Image } from 'semantic-ui-react';
+import React from 'react'
+import {
+  Container,
+  Segment,
+  Header,
+  Grid,
+  Icon,
+  Image,
+} from 'semantic-ui-react'
 
-function AkunSaya() {
-	return (
-		<Container>
-			<Header as="h3">
-				<Icon name="user outline" size="large" />
-				<Header.Content>dataUsername</Header.Content>
-			</Header>
-			<Segment>
-				<Header as="h2" textAlign="center" style={styles.marginHeader}>
-					Akun Saya
-				</Header>
+function AkunSaya(props) {
+  return (
+    <Container>
+      <Segment>
+        <Header as="h2" textAlign="center" style={styles.marginHeader}>
+          <Icon name="user outline" size="mini" />
+          Akun Saya
+        </Header>
 
-				<Grid columns={2}>
-					<Grid.Column width={5}>
-						<Segment style={{ padding: '15px', marginLeft: '20px' }}>
-							<Image src="https://placeimg.com/120/120/any" fluid />
-						</Segment>
-					</Grid.Column>
-					<Grid.Column width={11}>
-						<div style={{ marginLeft: '20px' }}>
-							<Header as="h4" style={{ marginBottom: '20px' }}>
-								Data Akun
-							</Header>
-							<Grid columns={2}>
-								<Grid.Column width={5}>
-									<p style={{ marginBottom: '20px' }}>Username</p>
-									<p style={{ marginBottom: '20px' }}>Nama</p>
-									<p style={{ marginBottom: '20px' }}>Status Keanggotaan</p>
-								</Grid.Column>
-								<Grid.Column width={11}>
-									<p style={{ marginBottom: '20px' }}>dataUsername</p>
-									<p style={{ marginBottom: '20px' }}>dataNama</p>
-									<p style={{ marginBottom: '20px' }}>dataStatus</p>
-								</Grid.Column>
-							</Grid>
-						</div>
-					</Grid.Column>
-				</Grid>
-			</Segment>
-		</Container>
-	);
+        <Grid columns={2}>
+          <Grid.Column width={5}>
+            <Segment style={{ padding: '15px', marginLeft: '20px' }}>
+              <Image src="https://placeimg.com/120/120/any" fluid />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <div style={{ marginLeft: '20px' }}>
+              <Header as="h4" style={{ marginBottom: '20px' }}>
+                Data Akun
+              </Header>
+              <Grid columns={2}>
+                <Grid.Column width={5}>
+                  <p style={{ marginBottom: '20px' }}>Username</p>
+                  <p style={{ marginBottom: '20px' }}>Nama</p>
+                  <p style={{ marginBottom: '20px' }}>Status Keanggotaan</p>
+                </Grid.Column>
+                <Grid.Column width={11}>
+                  <p style={{ marginBottom: '20px' }}>
+                    {props.pengguna.username}
+                  </p>
+                  <p style={{ marginBottom: '20px' }}>{props.pengguna.nama}</p>
+                  <p style={{ marginBottom: '20px' }}>
+                    {props.pengguna.keanggotaan ? 'Anggota' : 'Pengguna'}
+                  </p>
+                </Grid.Column>
+              </Grid>
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    </Container>
+  )
 }
 
 const styles = {
-	marginHeader: {
-		marginTop: '30px',
-		marginBottom: '50px'
-	},
-	marginContent: {
-		marginLeft: '50px'
-	}
-};
+  marginHeader: {
+    marginTop: '30px',
+    marginBottom: '50px',
+  },
+  marginContent: {
+    marginLeft: '50px',
+  },
+}
 
-export default AkunSaya;
+export default AkunSaya
--- src/pages/Profil/index.jsx
+++ src/pages/Profil/index.jsx
@@ -1,78 +1,104 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Container, Grid } from 'semantic-ui-react';
-import { UserContext } from '../../App';
-import axios from 'axios';
-import SidebarProfil, { listActiveSidebarItem } from './SidebarProfil';
-import Nav, { listActiveItem } from './Nav';
-import PesananList from './PesananList';
-import UangSaya from './UangSaya';
-import AkunSaya from './AkunSaya';
+import React, { useState, useEffect, useContext } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+import { UserContext } from '../../App'
+import axios from 'axios'
+import SidebarProfil, { listActiveSidebarItem } from './SidebarProfil'
+import { listActiveItem } from './NavPesananSaya'
+import UangSaya from './UangSaya'
+import AkunSaya from './AkunSaya'
+import PesananSayaContainer from './PesananSayaContainer'
 
 function Profil(props) {
-	const context = useContext(UserContext);
-	let idPengguna;
-	const [ pengguna, setPengguna ] = useState();
-	const [ loading, setLoading ] = useState(false);
+  const context = useContext(UserContext)
+  let idPengguna
+  const [pengguna, setPengguna] = useState()
+  const [loading, setLoading] = useState(false)
 
-	const [ activeItem, setActiveItem ] = useState(listActiveItem.semua);
-	const [ activeSidebarItem, setactiveSidebarItem ] = useState(listActiveSidebarItem.akunSaya);
+  const [activeItem, setActiveItem] = useState(listActiveItem.semua)
+  const [activeSidebarItem, setactiveSidebarItem] = useState(
+    listActiveSidebarItem.akunSaya,
+  )
 
-	useEffect(() => {
-		setLoading(false);
-		if (!props.location.state) {
-			if (context.getPengguna()) {
-				idPengguna = context.getPengguna().id_pengguna;
-				axios.get(`http://localhost:8000/pengguna/${idPengguna}`).then((res) => {
-					setPengguna(res.data);
-					setLoading(true);
-				});
-			} else {
-				props.history.push('/');
-			}
-		} else {
-			axios
-				.get(`http://localhost:8000/pengguna/${props.location.state.id_pengguna}`)
-				.then((res) => setPengguna(res.data));
-			setLoading(true);
-		}
-	}, []);
-	return (
-		<Container style={(styles.marginSide, styles.paddingContainer)}>
-			{pengguna &&
-			loading && (
-				<Grid columns={2}>
-					<Grid.Row>
-						<Grid.Column width={3}>
-							<SidebarProfil setActiveItem={setactiveSidebarItem} activeItem={activeSidebarItem} />
-						</Grid.Column>
-						<Grid.Column width={13}>
-							{activeSidebarItem === listActiveSidebarItem.akunSaya && <AkunSaya />}
-							{activeSidebarItem === listActiveSidebarItem.pesananSaya && [
-								<Nav setActiveItem={setActiveItem} activeItem={activeItem} />,
-								activeItem === listActiveItem.semua && <PesananList />,
-								activeItem === listActiveItem.belumBayar && <PesananList />
-							]}
-							{activeSidebarItem === listActiveSidebarItem.uangSaya && <UangSaya />}
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			)}
-		</Container>
-	);
+  const [kumpulanTransaksi, setKumpulanTransaksi] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    if (!props.location.state) {
+      if (context.getPengguna()) {
+        idPengguna = context.getPengguna().id_pengguna
+        axios.get(`http://localhost:8000/pengguna/${idPengguna}`).then(res => {
+          setPengguna(res.data)
+          axios
+            .get('http://localhost:8000/transaksi', {
+              headers: { Authorization: `Bearer ${context.token}` },
+            })
+            .then(res => {
+              setKumpulanTransaksi(res.data)
+              console.log(res.data)
+              setLoading(false)
+            })
+        })
+      } else {
+        props.history.push('/')
+      }
+    } else {
+      axios
+        .get(
+          `http://localhost:8000/pengguna/${props.location.state.id_pengguna}`,
+        )
+        .then(res => {
+          setPengguna(res.data)
+          setLoading(false)
+        })
+    }
+  }, [])
+  return (
+    <Container style={(styles.marginSide, styles.paddingContainer)}>
+      {pengguna && !loading && (
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <SidebarProfil
+                pengguna={pengguna}
+                setActiveItem={setactiveSidebarItem}
+                activeItem={activeSidebarItem}
+              />
+            </Grid.Column>
+            <Grid.Column width={13}>
+              {activeSidebarItem === listActiveSidebarItem.akunSaya && (
+                <AkunSaya pengguna={pengguna} />
+              )}
+              {activeSidebarItem === listActiveSidebarItem.pesananSaya && (
+                <PesananSayaContainer
+                  kumpulanTransaksi={kumpulanTransaksi}
+                  setActiveItem={setActiveItem}
+                  activeItem={activeItem}
+                  listActiveItem={listActiveItem}
+                />
+              )}
+              {activeSidebarItem === listActiveSidebarItem.uangSaya && (
+                <UangSaya />
+              )}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      )}
+    </Container>
+  )
 }
 
 const styles = {
-	cardRow: {
-		marginTop: 15
-	},
-	marginSide: {
-		marginTop: 15
-	},
-	noMargin: {
-		marginTop: '0'
-	},
-	paddingContainer: {
-		padding: 14
-	}
-};
-export default Profil;
+  cardRow: {
+    marginTop: 15,
+  },
+  marginSide: {
+    marginTop: 15,
+  },
+  noMargin: {
+    marginTop: '0',
+  },
+  paddingContainer: {
+    padding: 14,
+  },
+}
+export default Profil
